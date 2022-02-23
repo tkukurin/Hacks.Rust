843193bd36ce056b2049ef9354e56ac3c99c3d58
Toni Kukurin <tkukurin@gmail.com>
2022-02-23 09:10:43 +0000

    reorder more logical

--- src/first.rs
+++ src/first.rs
@@ -1,40 +1,17 @@
 use std::mem;
 
-#[cfg(test)]
-mod test {
-    use super::List;
-
-    #[test]
-    fn basics() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
-
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        assert_eq!(list.peek(), Some(3));
-        assert_eq!(list.peek(), Some(3));
-        assert_eq!(list.pop(), Some(3));
-
-        assert_eq!(list.peek(), Some(2));
-        assert_eq!(list.peek(), Some(2));
-        assert_eq!(list.pop(), Some(2));
-
-        list.push(4);
-        assert_eq!(list.pop(), Some(4));
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
-    }
+pub struct List {
+    head: Link
 }
 
-impl Drop for List {
-    fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
-        }
-    }
+pub enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+pub struct Node {
+    value: i32,
+    next: Link
 }
 
 impl List {
@@ -82,17 +59,39 @@ impl List {
     }
 }
 
-pub struct List {
-    head: Link
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
 }
 
-pub enum Link {
-    Empty,
-    More(Box<Node>),
-}
+#[cfg(test)]
+mod test {
+    use super::List;
 
-pub struct Node {
-    value: i32,
-    next: Link
-}
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
 
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(3));
+        assert_eq!(list.peek(), Some(3));
+        assert_eq!(list.pop(), Some(3));
+
+        assert_eq!(list.peek(), Some(2));
+        assert_eq!(list.peek(), Some(2));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
