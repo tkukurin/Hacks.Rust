cbc6d40ba7335b4c8a6b75a674caf288e2ccbb3d
Toni Kukurin <tkukurin@gmail.com>
2022-02-23 07:40:22 +0000

    actually implement some test

--- src/first.rs
+++ src/first.rs
@@ -1,8 +1,30 @@
 use std::mem;
 
+#[cfg(test)]
 mod test {
+    use super::List;
+
     #[test]
     fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(3));
+        assert_eq!(list.peek(), Some(3));
+        assert_eq!(list.pop(), Some(3));
+
+        assert_eq!(list.peek(), Some(2));
+        assert_eq!(list.peek(), Some(2));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
 
