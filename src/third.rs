1812cad04014e0cb3ad5840fde5570032e9f399c
Toni Kukurin <tkukurin@gmail.com>
2022-02-23 17:14:56 +0000

    Third with  (shared )

---
+++ src/third.rs
@@ -0,0 +1,14 @@
+// like Box, but shared
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
