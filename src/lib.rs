a8371947487884c8ca2d500a8224703e00864527
Toni Kukurin <tkukurin@gmail.com>
2022-02-23 09:25:50 +0000

    Link == Option impl quick

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 
 pub mod first;
+pub mod second;
 
 
 #[cfg(test)]
---
+++ src/second.rs
@@ -0,0 +1,45 @@
+
+use std::mem;
+
+pub struct List {
+    head: Link
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    elem: i32,
+    next: Link
+}
+
+impl List {
+    fn new() -> Self {
+        List { head: None }
+    }
+
+    fn push(&mut self, elem: i32) {
+        self.head = Link::Some(Box::new(Node {
+            elem: elem,
+            next: self.head.take(),
+        }))
+    }
+
+    fn pop(&mut self) -> Option<i32> {
+        match self.head.take() {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur = self.head.take();
+        while let Some(mut node) = cur {
+            cur = node.next.take();
+        }
+    }
+}
