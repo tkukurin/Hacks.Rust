7a10b845d0959058bc7d23e6f5080012a4e3119d
Toni Kukurin <tkukurin@gmail.com>
2022-02-23 19:14:14 +0000

    basic tests for 3rd

--- src/third.rs
+++ src/third.rs
@@ -24,5 +24,37 @@ impl<T> List<T> {
         })) }
     }
 
+    pub fn tail(&self) -> List<T> {
+        // `and_then` == flatmap. `x.and_then(f1).and_then(f2)` short for:
+        // match x {
+        //   None => None,
+        //   val => match val.map(f1) {
+        //     None => None,
+        //     val => val.map(f2),
+        //   }
+        // }
+        // (note that n.next == Option<T>)
+        List { head: self.head.as_ref().and_then(|n| n.next.clone()) }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|n| &n.elem)
+    }
 }
 
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let l1 = List::new();
+        assert_eq!(l1.head(), None);
+
+        let l2 = l1.prepend(1).prepend(2).prepend(3);
+        assert_eq!(l1.head(), None);
+        assert_eq!(l2.head(), Some(&3));
+
+        assert_eq!(l2.tail().head(), Some(&2));
+    }
+}
