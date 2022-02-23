eeb12317851ec3eebb2803b5ae36623f9d69e25b
Toni Kukurin <tkukurin@gmail.com>
2022-02-23 07:36:11 +0000

    Some initial push/pop/peek impl

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Rust with lists
+
+Some [unofficial Rust tutorial](https://rust-unofficial.github.io/).
+
---
+++ src/first.rs
@@ -0,0 +1,61 @@
+use std::mem;
+
+impl List {
+    fn new() -> Self {
+        return List { head: Link::Empty };
+    }
+
+    fn push(&mut self, value: i32) {
+        let node = Node {
+            value: value,
+            next: mem::replace(&mut self.head, Link::Empty),
+        };
+        self.head = Link::More(Box::new(node));
+    }
+
+    fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.value)
+            }
+        }
+
+        // Ugly way of doing this
+        // let result;
+        // match &mut self.head {
+        //     Link::Empty => {
+        //         result = Option::None;
+        //     },
+        //     Link::More(node) => {
+        //         result = Option::Some(node.value);
+        //         self.head = mem::replace(&mut node.next, Link::Empty);
+        //     }
+        // }
+        // result
+    }
+
+    fn peek(&mut self) -> Option<i32> {
+        // note the lack of semicolons (implicit return)
+        match &self.head {
+            Link::Empty => Option::None,
+            Link::More(node) => Option::Some(node.value)
+        }
+    }
+}
+
+pub struct List {
+    head: Link
+}
+
+pub enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+pub struct Node {
+    value: i32,
+    next: Link
+}
+
