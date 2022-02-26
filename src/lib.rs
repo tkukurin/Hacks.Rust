93b3581eea02fa069090dc340fa9399b92ff5c9f
Toni Kukurin <tkukurin@gmail.com>
2022-02-26 10:35:36 +0000

    rm unnecessary test + fifth

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,5 @@ pub mod first;
 pub mod second;
 pub mod third;
 pub mod fourth;
+pub mod fifth;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
